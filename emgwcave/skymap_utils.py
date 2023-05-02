5a9434facfbab5f4228e2080e5fbf7d138983426
viraj21197 <karambelkarvraj21197@gmail.com>
2023-05-01 17:58:07 -0700

    fix so that works with GRB

--- emgwcave/skymap_utils.py
+++ emgwcave/skymap_utils.py
@@ -2,10 +2,9 @@ import healpy as hp
 import numpy as np
 from ligo.skymap.postprocess import find_greedy_credible_levels
 from astropy.io import fits
-from astropy.table import Table
 from scipy.stats import norm
 import os
-from astropy.table import Column
+from astropy.table import Table
 
 
 def get_flattened_skymap_path(skymap_path):
@@ -16,13 +15,13 @@ def read_lvc_skymap_fits(filename):
     h = fits.open(filename)
     data = Table(h[1].data)
 
-    prob = np.array(data['PROBDENSITY'])
+    prob_density = np.array(data['PROBDENSITY'])
     distmu = np.array(data['DISTMU'])
     distsigma = np.array(data['DISTSIGMA'])
     distnorm = np.array(data['DISTNORM'])
 
     header = h[1].header
-    return prob, distmu, distsigma, distnorm, header
+    return prob_density, distmu, distsigma, distnorm, header
 
 
 def read_fermi_skymap_fits(filename):
@@ -30,16 +29,14 @@ def read_fermi_skymap_fits(filename):
     data = Table(h[1].data)
 
     uniq = np.array(data['UNIQ'])
-    prob = np.array(data['PROBDENSITY'])
+    prob_density = np.array(data['PROBDENSITY'])
 
     header = h[1].header
-    return uniq, prob, header
+    return uniq, prob_density, header
 
 
 def get_credible_levels(prob):
-    npix = len(prob)
     credible_levels = find_greedy_credible_levels(prob)
-    nside = hp.npix2nside(npix)
     return credible_levels
 
 
@@ -48,7 +45,7 @@ def dist_function(r, distnorm, distmu, distsigma):
     return dp_dr
 
 
-def flatten_skymap(skymap_path, flatten_file_path, update_object_name=True):
+def flatten_skymap(skymap_path, flatten_file_path, update_object_name=False):
     cmd = f'ligo-skymap-flatten {skymap_path} {flatten_file_path}'
     print(cmd)
     os.system(cmd)
@@ -59,22 +56,29 @@ def flatten_skymap(skymap_path, flatten_file_path, update_object_name=True):
         h.writeto(flatten_file_path, overwrite=True)
 
 
-def read_flattened_skymap(flatten_file_path):
-
-    (prob, distmu, distsigma, distnorm), hdr = hp.fitsfunc.read_map(flatten_file_path,
-                                                                    verbose=False,
-                                                                    partial=False,
-                                                                    field=range(4),
-                                                                    h=True)
+def read_flattened_skymap(flatten_file_path, num_fields=1):
+    try:
+        (prob, distmu, distsigma, distnorm), hdr = hp.fitsfunc. \
+            read_map(flatten_file_path,
+                     verbose=False,
+                     partial=False,
+                     field=4,
+                     h=True)
+    except IndexError:
+        prob, hdr = hp.fitsfunc.read_map(flatten_file_path,
+                                         verbose=False,
+                                         partial=False,
+                                         field=range(1),
+                                         h=True)
+        distmu, distsigma, distnorm = None, None, None
 
     return (prob, distmu, distsigma, distnorm), hdr
 
 
 def in_skymap(skymap_prob: np.ndarray,
-              ra_obj: list,
-              dec_obj: list,
+              ra_obj: list | np.ndarray,
+              dec_obj: list | np.ndarray,
               probability: float = 0.9):
-
     # sortinds = np.argsort(skymap_prob)
     # skymap_prob = skymap_prob[sortinds]
     # cumprob = np.cumsum(skymap_prob)
@@ -83,6 +87,7 @@ def in_skymap(skymap_prob: np.ndarray,
     # pix = hp.ang2pix(nside, ra_obj, dec_obj, lonlat=True)
     # return pix in top_indices
     npix = len(skymap_prob)
+    print(f"Npix : {skymap_prob}")
     nside = hp.npix2nside(npix)
     ipix = hp.ang2pix(nside, ra_obj, dec_obj, lonlat=True)
     credible_levels = find_greedy_credible_levels(skymap_prob)
